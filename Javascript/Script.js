4472117dc4fac11c7a949412029b2a1f4d0d8f94
DMU-P2529254 <78545241+DMU-P2529254@users.noreply.github.com>
2022-05-23 13:45:47 +0100

    Update Script.js

--- Javascript/Script.js
+++ Javascript/Script.js
@@ -1,14 +1,14 @@
-
+ <!-- Below, the W3Schools' (W3 Schools (2022)How TO - Overlay, How To Create an Overlay Effect. Available at:https://www.w3schools.com/howto/howto_css_overlay.asp (Accessed: April 28, 2022)) was utilised, the below javascript sourced directly from W3schools.-->
 
 function on() {
   document.getElementById("TextBox").style.display = "block";
-}
+}<!--here, if the function notices an activation of the already monitored asset with the "on()" attribute, it will find the 'TextBox' asset and set it's style to be block, from a prior state of none, making it visible-->
 
 function off() {
   document.getElementById("TextBox").style.display = "none";
 };;
 
-
+<!--comparatively, the "function off()" inverts this, making the prior visibile element invisible.-->
 function on1() {
   document.getElementById("TextBox1").style.display = "block";
 }
@@ -27,11 +27,13 @@ function on2() {
 function off2() {
   document.getElementById("TextBox2").style.display = "none";
 };;
+<!--this is then repeated a number of times for a variety of text boxes-->
 
 
 
-
+<!--next was the frame by frame Javascript animation, utilisign the individually exported .SVG frames-->
 let HomePageImages = [
+ <!--Here, the Let Array is then utilised, allowing for a variable to be declreaded within this block expression, the following code both sourced from and learnt from "bhawnaatrish (2021) How to create Frame by Frame Animation using CSS and JavaScript ?. Available at: https://www.geeksforgeeks.org/how-to-create-frame-by-frame-animation-using-css-and-javascript/?ref=rp#:~:text=We%20can%20use%20the%20JavaScript,interval%20gaps%20in%20between%20them. (Accessed: April 25 2022)"-->
 "Images/HeaderAnimations/WomanWaveAnim/womanwave0001.svg",
 "Images/HeaderAnimations/WomanWaveAnim/womanwave0002.svg",
 "Images/HeaderAnimations/WomanWaveAnim/womanwave0003.svg",
@@ -41,6 +43,7 @@ let HomePageImages = [
 "Images/HeaderAnimations/WomanWaveAnim/womanwave0007.svg",
 "Images/HeaderAnimations/WomanWaveAnim/womanwave0008.svg",
 "Images/HeaderAnimations/WomanWaveAnim/womanwave0009.svg",
+  <!--originally, a 'var' array was present, but this was echnaged for the let attribute.-->
 
 "Images/HeaderAnimations/WomanWaveAnim/womanwave0010.svg",
 "Images/HeaderAnimations/WomanWaveAnim/womanwave0011.svg",
@@ -145,10 +148,13 @@ let HomePageImages = [
 ];
 
 setInterval("Animate()", 24);
+<!--interval determines the frame rate of the individual exported shots-->
 let x = 0;
-
+<!--the "function Animate()" then animated the selected frames, utilising the ''-->
 function Animate() {
   document.getElementById("img").src = HomePageImages[x]
+  <!-- this then retrieves the indicated element ("img"), and assigns its source as the animation, allowing for the frmae by frame SVG animation to be utilised-->
   x++;
   if (HomePageImages.length == x) {
       x = 0}};
+<!--Finally, the 'if' element is inserted, establishing that the end of an array has been reached if the length is equal to zero-->

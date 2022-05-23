5a961ced0fd7a175e8062556aa432459d06701a0
DMU-P2529254 <78545241+DMU-P2529254@users.noreply.github.com>
2022-05-23 11:19:15 +0100

    Initial commit

---
+++ Javascript/Script.js
@@ -0,0 +1,154 @@
+
+
+function on() {
+  document.getElementById("TextBox").style.display = "block";
+}
+
+function off() {
+  document.getElementById("TextBox").style.display = "none";
+};;
+
+
+function on1() {
+  document.getElementById("TextBox1").style.display = "block";
+}
+
+function off1() {
+  document.getElementById("TextBox1").style.display = "none";
+};;
+
+
+
+
+function on2() {
+  document.getElementById("TextBox2").style.display = "block";
+}
+
+function off2() {
+  document.getElementById("TextBox2").style.display = "none";
+};;
+
+
+
+
+let HomePageImages = [
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0001.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0002.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0003.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0004.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0005.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0006.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0007.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0008.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0009.svg",
+
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0010.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0011.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0011.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0012.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0013.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0014.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0015.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0016.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0017.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0018.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0019.svg",
+
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0020.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0021.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0022.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0023.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0024.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0025.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0026.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0027.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0028.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0029.svg",
+
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0030.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0031.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0032.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0033.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0034.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0035.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0036.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0037.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0038.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0039.svg",
+
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0040.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0041.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0042.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0043.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0044.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0045.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0046.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0047.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0048.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0049.svg",
+
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0050.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0051.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0052.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0053.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0054.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0055.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0056.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0057.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0058.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0059.svg",
+
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0050.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0051.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0052.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0053.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0054.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0055.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0056.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0057.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0058.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0059.svg",
+
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0060.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0061.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0062.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0063.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0064.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0065.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0066.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0067.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0068.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0069.svg",
+
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0070.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0071.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0072.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0073.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0074.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0075.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0076.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0077.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0078.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0079.svg",
+
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0080.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0081.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0082.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0083.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0084.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0085.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0086.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0087.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0088.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0089.svg",
+"Images/HeaderAnimations/WomanWaveAnim/womanwave0090.svg",
+];
+
+setInterval("Animate()", 24);
+let x = 0;
+
+function Animate() {
+  document.getElementById("img").src = HomePageImages[x]
+  x++;
+  if (HomePageImages.length == x) {
+      x = 0}};
